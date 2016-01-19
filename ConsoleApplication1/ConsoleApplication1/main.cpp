4f172d8c85ed7493dfb0a3103adef2350571b719
Armand Moulis <armand@moulis.se>
2016-01-19 10:25:13 +0100

    First visual studios project test

---
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -0,0 +1,18 @@
+
+#include "conio.h"
+#include <iostream>
+
+#include <opencv2\opencv.hpp>
+
+using namespace cv;
+void main(void)
+{
+	Mat img = cv::imread("D:\\Testdata\\lena.png");
+	namedWindow("MyWindow", CV_WINDOW_AUTOSIZE); //create a window with the name "MyWindow"
+	imshow("MyWindow", img); //display the image which is stored in the 'img' in the "MyWindow" window
+	waitKey(0);
+	destroyWindow("MyWindow"); //destroy the window with the name, "MyWindow"
+	std::cout << "Hej hej";
+
+	_getch();
+}
